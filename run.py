9d6fdbda0b3e8511c56ed9070bd326b4b1de1c86
Nick Lalande <nick@lalande.me>
2019-11-23 16:54:22 -0500

    Added:  assignmentFilter(...)
    Signed-off-by: Nick Lalande <nick@lalande.me>

--- run.py
+++ run.py
@@ -1,14 +1,8 @@
 from calendar import getDate, getSchedule, scheduleCheck
 from config import configGet
 
-
-def main():
-    config = configGet()
-    today = getDate()
-    today = "2019-11-21"
-    # print(config)
-    print("\r\n\tGreat {}!  Today's date is {}...".format(config["firstName"], today))
-    # webbrowser.open('file://' + os.path.realpath(FILENAME))
+def assignmentsFilter(dateString, config):
+    today = dateString
     scheduleData = getSchedule()
     dates = scheduleData["list"]
     courseData = scheduleData["dictionary"]
@@ -19,5 +13,14 @@ def main():
             print("\t\tkey:  ", key, ", value:  {}".format(assignment[key]))
     else:
         print("\r\n\tThere's no assignment today...")
+
+def main():
+    config = configGet()
+    # webbrowser.open('file://' + os.path.realpath(FILENAME))
+    today = getDate()
+    today = "2019-11-21"
+    print("\r\n\tGreat {}!  Today's date is {}...".format(config["firstName"], today))
+    assignmentsFilter(today, config)
+
 
 main()
