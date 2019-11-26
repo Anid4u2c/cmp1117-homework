88e88d4656502ba76002528f011e4ea485b97d01
Nick Lalande <nick@lalande.me>
2019-11-26 14:27:43 -0500

    Worked in group on assignmentFilter(...)
    Signed-off-by: Nick Lalande <nick@lalande.me>

--- run.py
+++ run.py
@@ -9,8 +9,14 @@ def assignmentsFilter(dateString, config):
     if scheduleCheck(today, dates) == True:
         assignment = courseData[today]
         print("\r\n\tThere's an assignment today:  ")
-        for key in assignment.keys():
-            print("\t\tkey:  ", key, ", value:  {}".format(assignment[key]))
+        print("\t\tFor {}, in week {}".format(today, assignment["Week"]))
+        for key in sorted(assignment.keys()):
+            if key not in ["Date", "Week", "Class", "Day"]:
+                print("\t\t", key + ":  ", assignment[key])
+        response = input("\r\n\tWould you like to create a file for this assignment ('N' = 'no', 'Y' = 'yes'):  ")
+        if response.upper() == "Y" and len(response) > 0:
+            print("\t\tOkay, we'll create a file named:  ch{}.".format(assignment["Chapter"].lower()))
+
     else:
         print("\r\n\tThere's no assignment today...")
 
@@ -18,7 +24,7 @@ def main():
     config = configGet()
     # webbrowser.open('file://' + os.path.realpath(FILENAME))
     today = getDate()
-    today = "2019-11-21"
+    #today = "2019-11-21"
     print("\r\n\tGreat {}!  Today's date is {}...".format(config["firstName"], today))
     assignmentsFilter(today, config)
 
