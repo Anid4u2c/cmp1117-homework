9e3382f0b1233f100582ef6e284af5ea31a2e569
anid4u2c <nick@djn.ca>
2019-12-01 21:47:48 -0500

    Cleaned up configSet(...) and reorganized code.
    Signed-off-by: anid4u2c <nick@djn.ca>

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ import json
 from calendar import getDateTime
 
 FILENAME = ".config"
+FOLDERS = ("homework", "labs")
 KEY_MAP = {"firstName": "First Name", "lastName": "Last Name",
            "studentId": "Student ID", "dateTime": "Last Updated"}
-TEMPLATE = ("dateTime", "firstName", "lastName", "studentId")
+TEMPLATE = ("dateTime", "files", "firstName", "lastName", "studentId")
 
 def configEdit(config):
     response = input("\r\n\tWould you like to edit the current configuration (N = 'No', Y = 'Yes'):  ")
@@ -31,7 +32,7 @@ def configOutput(config):
     try:
         output = "\r\n\tHere is the current configuration for this program:\r\n"
         for key in config.keys():
-            if key != "dateTime":
+            if key not in ["dateTime", "files"]:
                 output += "\r\n\t{}:  {}".format(mapKey(key), config[key])
         return config
     except AttributeError:
@@ -46,10 +47,6 @@ def configOutput(config):
 def configSet(config):
     print("\r\n\tSetting or updating the user configuration for this program:\r\n")
     file = open(FILENAME, "w")
-    # config += "dateTime:  " + config["dateTime"] + "\n"
-    # config += "firstName:  " + config["firstName"] + "\n"
-    # config += "lastName:  " + config["lastName"] + "\n"
-    # config += "studentId:  " + config["studentId"] + "\n"
     file.write(json.dumps(config))
     file.close()
     return config
