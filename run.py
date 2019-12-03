7ace4c4c9fa50b840974cbde52a2ae3e90d03394
anid4u2c <nick@djn.ca>
2019-12-02 22:00:02 -0500

    Updated project, added additional files, and enhanced functions in run.py
    Signed-off-by: anid4u2c <nick@djn.ca>

--- run.py
+++ run.py
@@ -1,12 +1,40 @@
 from calendar import getDate, getSchedule, scheduleCheck
 from config import configGet
+from fileActions import fileCreate, filesList, fileNameGet, fileRename, OPTIONS_FILE
+from helpers import printOptions
+
+OPTIONS_NAME = {1: "Allow the application to name the file for you.", 2: "Specify a name for your file.", 3: "GO BACK"}
+OPTIONS = {1: "Create a file for an assignment", 2: "List all assignment files", 3: "Rename a assignment file",
+           4: "QUIT"}
+
+def actionGet(option):
+    if option is None:
+        option = ""
+    if option in OPTIONS.keys():
+        print("\t\tOkay, let's " + OPTIONS[option].lower() + "...")
+        if option == 1:
+            fileType = OPTIONS_FILE[printOptions(OPTIONS_FILE)]
+            fileNameOption = printOptions(OPTIONS_NAME)
+            if fileNameOption == 2:
+                fileName = input("\r\n\tWhat would you like to name your {} file?  ".format(fileType))
+                fileType += "s"
+            else:
+                fileName = fileNameGet(fileType)
+            fileCreate(fileName, fileType)
+        elif option == 2:
+            for folder in ("homework", "labs"):
+                filesList(folder)
+        elif option == 3:
+            fileRename()
+        else:
+            quit()
 
 def assignmentsFilter(dateString, config):
     today = dateString
     scheduleData = getSchedule()
     dates = scheduleData["list"]
     courseData = scheduleData["dictionary"]
-    if scheduleCheck(today, dates) == True:
+    if scheduleCheck(today, dates):
         assignment = courseData[today]
         print("\r\n\tThere's an assignment today:  ")
         print("\t\tFor {}, in week {}".format(today, assignment["Week"]))
@@ -16,15 +44,15 @@ def assignmentsFilter(dateString, config):
         response = input("\r\n\tWould you like to create a file for this assignment ('N' = 'no', 'Y' = 'yes'):  ")
         if response.upper() == "Y" and len(response) > 0:
             print("\t\tOkay, we'll create a file named:  ch{}.".format(assignment["Chapter"].lower()))
-
     else:
         print("\r\n\tThere's no assignment today...")
+    actionGet(printOptions(OPTIONS))
 
 def main():
     config = configGet()
     # webbrowser.open('file://' + os.path.realpath(FILENAME))
     today = getDate()
-    #today = "2019-11-21"
+    # today = "2019-11-21"
     print("\r\n\tGreat {}!  Today's date is {}...".format(config["firstName"], today))
     assignmentsFilter(today, config)
 
