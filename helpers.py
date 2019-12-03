15157101cfb54ac7c4ae681568920d99b258d39c
anid4u2c <nick@djn.ca>
2019-12-02 21:39:13 -0500

    Created file that includes printOptions(...)
    Signed-off-by: anid4u2c <nick@djn.ca>

---
+++ helpers.py
@@ -0,0 +1,11 @@
+def printOptions(options):
+    if options is None:
+        options = OPTIONS
+    print("\r\n\tHere are your options:")
+    for key in options .keys():
+        print("\t\t{} : {}".format(key, options [key]))
+    try:
+        return int(input("\r\n\tWhich option do you choose?  "))
+    except ValueError:
+        print("\r\b\n\t\tERROR: Response cannot be empty, and must be in the list of options.")
+        printOptions(options)
