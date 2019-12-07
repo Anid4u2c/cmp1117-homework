0f2b1babdc0cb8f357334c67806a7d2918e349e8
anid4u2c <nick@djn.ca>
2019-12-07 02:19:04 -0500

    Added addOption(...) to include alternate options like "GO BACK" and "QUIT"
    Signed-off-by: anid4u2c <nick@djn.ca>

--- helpers.py
+++ helpers.py
@@ -7,3 +7,11 @@ def printOptions(options):
     except ValueError:
         print("\r\b\n\t\tERROR: Response cannot be empty, and must be in the list of options.")
         printOptions(options)
+
+def addOption(dictionary, option):
+    if option is None:
+        dictionary = {}
+    key = len(dictionary.keys())+1
+    if option not in dictionary.values():
+        dictionary[key] = option
+    return dictionary
